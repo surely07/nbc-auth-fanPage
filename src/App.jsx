19fd55221ac313fd800e79ffd6c4e5fe29bf1e59
Kwon Bora <sweetpt88@gmail.com>
2023-12-04 10:27:55 +0900

    feat: 오류

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import Router from "shared/Router";
 import GlobalStyle from "style/GlobalStyle";
 import { __getLetters } from "redux/modules/lettersSlice";
-import { __getUserData } from "redux/modules/authSlice";
+// import { __getUserData } from "redux/modules/authSlice";
 
 function App() {
   const dispatch = useDispatch();
@@ -13,7 +13,6 @@ function App() {
     if (!accessToken) return;
 
     dispatch(__getLetters());
-    dispatch(__getUserData());
   }, [dispatch]);
 
   return (
